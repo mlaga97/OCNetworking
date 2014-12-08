27a68e4e3479a46d0be61716b21d8d9b2ed1f02c
Luc Lagarde <mlaga97@gmail.com>
2014-12-08 10:08:01 -0600

    add bare minimum functionality to mlaga97.net.packet.receive

--- src/mlaga97/net/packet/receive.lua
+++ src/mlaga97/net/packet/receive.lua
@@ -1,11 +1,41 @@
 local modname, path = ...
+local event = require("event")
+local checkType = require("mlaga97.util.checkType")
 local modError = require("mlaga97.util.modError")
 
-return function( requested_address, requested_port )
+local function filter( port, address, requested_port, requested_address )
+  if port == requested_port and address == requested_address then
+    return true
+  end
+end
+
+return function(requested_port, requested_address)
+  -- Error check types
+  checkType( requested_port or -1, "number", "Requested port" )
+  checkType( requested_address or "", "string", "Requested address" )
+  
   while true do
-    local type, this_computer, address, port, distance, ... = event.pull( "modem_message" )
-    if address == requested_address and port = requested_port then
-      return ...
+    -- Wait for modem_message event
+    local message = {event.pull("modem_message")}
+    
+    -- Is the packet one we are looking for?
+    if filter( message[4], message[3], requested_port, requested_address ) then
+      -- Excellent, lets return the message.
+      
+      -- Do to a weird 'feature' of the language, it is entirely necessary to
+      -- create an entire new table to dump the wanted variables into just
+      -- so that we can immediately unpack them before returning.
+      local toReturn = {}
+      
+      -- Find the variables we are looking for and put them in a table.
+      for pos = 6,#message do
+        table.insert(toReturn,message[pos])
+      end
+      
+      -- Return all of the variables!!!
+      return table.unpack(toReturn)
     end
+    
+    -- Nope? Alright, let's try again.
   end
 end
